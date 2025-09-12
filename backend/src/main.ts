da088eb314e7fa9d6565e29570f614d474b35f1c
productname <info@rocket.name>
2025-09-12 16:42:29 +0300

    feat: update main

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,19 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import 'dotenv/config';
+import { Logger } from '@nestjs/common';
+
+const logger = new Logger('App');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/afisha');
   app.enableCors();
-  await app.listen(3000);
+
+  const hostname = process.env.HOST || '0.0.0.0';
+  const port = process.env.PORT || 3000;
+  await app.listen(port, hostname);
+  logger.log(`Application is running on: http://${hostname}:${port}`);
 }
+
 bootstrap();
